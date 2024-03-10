7eef02cb04b231cb45619c4a6445ef06a19fa854
SAIKIRANPATNANA <saikiranpatnana5143@gmail.com>
2024-03-10 22:14:53 +0530

    updated

--- LAB-2/JavaArrayList.java
+++ LAB-2/JavaArrayList.java
@@ -1,13 +1,16 @@
-// import java.util.ArrayList;
-// public class JavaArrayList{
-//     public static void main(String[] args){
-//         ArrayList li = new ArrayList();
-//         for(int i=1; i<=10; i++){
-//             li.add(i);
-//         }for(int i=1; i<=10; i++){
-//             System.out.print(li[i]+" ");
-
-//         }
-//     }
-    
-// }
+import java.util.ArrayList;
+public class JavaArrayList{
+    public static void main(String[] args){
+        ArrayList<Integer> li = new ArrayList<Integer>();
+        for(int i=1; i<=10; i++){
+            li.add(i);
+        }System.out.println(li);
+        li.add(11);
+        System.out.println(li);
+        System.out.println(li.indexOf(5));
+        System.out.println(li.lastIndexOf(12));
+        System.out.println(li.isEmpty());
+        li.clear();
+        System.out.print(li);
+    }
+}
---
+++ LAB-2/JavaLinkedList.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+public class JavaLinkedList{
+    public static void main(String[] args){
+        LinkedList<Integer> li = new LinkedList<Integer>();
+        for(int i=1; i<=10; i++){
+            li.add(i);
+        }System.out.println(li);
+        li.add(11);
+        System.out.println(li);
+        System.out.println(li.get(1));
+        li.remove(1);
+        System.out.println(li);
+        System.out.println(li.isEmpty());
+        System.out.println(li.size());
+        System.out.println(li.getFirst());
+        System.out.print(li.getLast());
+        System.out.print(li);
+    }
+}
