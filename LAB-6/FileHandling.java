94d4795ffced753b8d86e4c3f4c171f3a1577360
SAIKIRANPATNANA <saikiranpatnana5143@gmail.com>
2024-04-18 17:20:13 +0530

    updated

---
+++ LAB-6/FileHandling.java
@@ -0,0 +1,99 @@
+import java.io.*;
+public class FilesTest {
+    public static void main(String[] args) {
+    	try {
+//    	File f1=new File("E:\\jaya");
+//         File f=new File(f1,"jk.txt");
+//         File f=new File("E:\\jaya\\jk.txt");
+//         if(f.exists())
+//        	 System.out.println("file already existed");
+//         else
+//         {
+    		
+         	//System.out.println(f.createNewFile());
+//         }
+//    		File f=new File("E:\\jaya");
+//    		String[] s=f.list();
+//    		for (String string : s) {
+//    			File f1=new File(string);
+//    			if(f1.isDirectory())
+//    				System.out.println("directory:"+string);
+//    			else
+//    				System.out.println("files:"+string);
+//			}
+    	
+    		//Reading  of data
+    		//FileInputStream fis=new FileInputStream("E:\\jaya\\jk.txt");
+    		
+//    		File f=new File("E:\\jaya\\jk.txt");
+//    		FileInputStream fis=new FileInputStream(f);
+//    		int i=0;
+//    		while((i=fis.read())!=-1)
+//    		{
+//    		System.out.println((char)i);
+//    		}
+    		
+//    		FileOutputStream fos=new FileOutputStream("E:\\jaya\\jk.txt",true);
+////    		fos.write(97);
+//    		String s="hai hello welcome";
+//    		byte[] b=s.getBytes();
+//    		fos.write(b);
+//    		System.out.println("program ended successfully");
+    		
+//    		File f=new File("E:\\jaya\\jk.txt");
+//    		FileReader fr=new FileReader(f);
+//    		char[] c=new char[1000];
+//    		char[] c=new char[(int)f.length()];
+//    		fr.read(c);
+//    		
+//    		System.out.println(c);
+    		
+//    		int i=0;
+//    		while((i=fr.read())!=-1)
+//    		{
+//    		System.out.println((char)i);
+//    		}
+    		
+//    		FileWriter fw=new FileWriter("E:\\jaya\\jk.txt",true);
+////    		fw.write(97);
+////    		fw.write('p');
+////    		fw.write("welcome to cse");
+//    		char[] ch= {'r','g','u','k','t'};
+//    		fw.write(ch,3,2);
+//    		fw.flush();
+//    		fw.close();
+    		
+//    		FileReader fr=new FileReader("E:\\jaya\\jk.txt");
+//    		BufferedReader br=new BufferedReader(fr);
+//    		
+//    		String l=br.readLine();
+//    		while(l!=null)
+//    		{
+//    			System.out.println(l);
+//    			l=br.readLine();
+//    		}
+//    		FileWriter fw=new FileWriter("E:\\jaya\\jk.txt");
+//    		BufferedWriter bw=new BufferedWriter(fw);
+//    		bw.write("jaya krishna");
+//    		bw.write("\n");
+//    		bw.write("hello");
+////    		bw.write(97.5);
+//    		bw.flush();
+//    		System.out.println("success");
+//    		bw.close();
+//    		
+//    		PrintWriter pw=new PrintWriter("E:\\jaya\\jk.txt");f
+//    		pw.print(10.5);
+//    		pw.print("hello");
+//    		pw.flush();
+//    		pw.close();
+    		
+    		
+    	}
+    	catch(Exception e)
+    	{
+    		System.out.println(e);
+    		
+    	}
+    }
+}
---
+++ LAB-6/FileHandlingPractice.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+public class FileHandlingPractice {
+    public static void main(String[] args) throws Exception {
+        File f1 = new File("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-4");
+        String [] files = f1.list();
+        for(String str: files){
+            File f2 = new File(str);
+            if(f2.isDirectory()){
+                System.out.println(str + "is a directory.");
+            }else if(f2.isFile()){
+                System.out.println(str + "is a file.");
+            }
+        }File f3 = new File("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt");
+        if(f3.exists()){
+            System.out.println("It already exists.");
+        }else{        
+            f3.createNewFile();
+        }FileInputStream fis = new FileInputStream("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt");
+        int i = 0;
+        while((i=fis.read())!=-1){
+            System.out.print((char)i);
+        }fis.close();
+        FileOutputStream fos = new FileOutputStream("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt",true);
+        String s = "Radha is loved by raju.\n";
+        byte b[] = s.getBytes();
+        fos.write(b);
+        fos.flush();
+        fos.close();
+        File f = new File("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt");
+        FileReader fr = new FileReader(f);
+        while((i=fr.read())!=-1){
+            System.out.print((char)i);
+        }char c[] = new char[(int)f.length()];
+        fr = new FileReader(f);
+        fr.read(c);
+        System.out.println(c);
+        fr.close();
+        FileWriter fw = new FileWriter("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt",true);
+        fw.write("Radha is loved by raju.\n");
+        fw.flush();
+        fw.close();
+        fw = new FileWriter("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt",true);
+        String st = "Radha is loved by raju.\n";
+        char ch[] = new char[st.length()];
+        st.getChars(0,st.length(),ch,0);
+        fw.write(ch,0,ch.length);
+        fw.flush();
+        fw.close();
+        fr = new FileReader("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while((line = br.readLine())!=null){
+            System.out.println(line);
+        }br.close();
+        fr.close();
+        fw = new FileWriter("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt",true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("Radha is loved by raju.\n");
+        bw.newLine();
+        bw.flush();
+        bw.close();
+        fw.close();
+        PrintWriter pw = new PrintWriter("/home/user/Downloads/OOPS_THROUGH_JAVA/LAB-6/sai.txt");
+        pw.write("Radha is loved by raju.\n");
+        pw.flush();
+        pw.close();
+    }
+}
